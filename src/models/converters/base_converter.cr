df467d94bbe94e7a1969c1848e58a06465ad7fa0
Michael Wagner <mdwranger@gmail.com>
2023-08-22 18:04:45 -0400

    fix: base converter not valid

--- src/models/converters/base_converter.cr
+++
@@ -1,3 +0,0 @@
-abstract class BaseConverter(T)
-  abstract def self.from_rs(rs : DB::ResultSet) : T
-end
--- src/models/converters/time_utc_converter.cr
+++ src/models/converters/time_utc_converter.cr
@@ -1,4 +1,4 @@
-class TimeUtcConverter < BaseConverter(Time)
+class TimeUtcConverter
   def self.from_rs(rs : DB::ResultSet) : Time
     Time::Format::ISO_8601_DATE_TIME.parse(rs.read(String))
   end
