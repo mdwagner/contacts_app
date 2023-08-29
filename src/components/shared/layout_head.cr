19242395b56c1f557b292d233b0b629f779927ec
Michael Wagner <mdwranger@gmail.com>
2023-08-28 22:39:59 -0400

    chore: wip

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -4,7 +4,7 @@ class Shared::LayoutHead < BaseComponent
   def render
     head do
       utf8_charset
-      title @page_title
+      title page_title
 
       app_css
       tailwind_css
@@ -15,38 +15,21 @@ class Shared::LayoutHead < BaseComponent
       csrf_meta_tags
       responsive_meta_tag
 
-      # TODO: add back live_reload_connect_tag after merged into Lucky core
-      # live_reload_connect_tag
-
-      tag "script" do
-        raw <<-JS
-        (function() {
-          var ws = new WebSocket("ws://#{Lucky::ServerSettings.host}:#{Lucky::ServerSettings.reload_port}");
-          ws.onmessage = function() {
-            setTimeout(function() {
-              location.reload();
-            }, 1000);
-          };
-        })();
-        JS
-      end
+      _live_reload_connect_tag
     end
   end
 
-  # TODO: run tailwindcss cli to generate compile-time classes for production
   private def tailwind_css
     tag "script",
       src: "https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"
   end
 
-  # TODO: vendor for production
   private def alpine_js
     tag "script",
       src: "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js",
       defer: "true"
   end
 
-  # TODO: vendor for production
   private def htmx_js
     tag "script",
       src: "https://unpkg.com/htmx.org@1.9.4",
@@ -60,7 +43,6 @@ class Shared::LayoutHead < BaseComponent
       crossorigin: "anonymous"
   end
 
-  # TODO: use readfile macro (src/css/app.css) for production
   private def app_css
     tag "style", media: "screen" do
       raw <<-CSS
@@ -68,6 +50,22 @@ class Shared::LayoutHead < BaseComponent
         display: none !important;
       }
       CSS
+    end
+  end
+
+  # TODO: add back live_reload_connect_tag after merged into Lucky core
+  private def _live_reload_connect_tag
+    tag "script" do
+      raw <<-JS
+      (function() {
+        var ws = new WebSocket("ws://#{Lucky::ServerSettings.host}:#{Lucky::ServerSettings.reload_port}");
+        ws.onmessage = function() {
+          setTimeout(function() {
+            location.reload();
+          }, 1000);
+        };
+      })();
+      JS
     end
   end
 end
--- src/pages/main_layout.cr
+++ src/pages/main_layout.cr
@@ -14,7 +14,7 @@ abstract class MainLayout
     html lang: "en" do
       mount Shared::LayoutHead, page_title: page_title
 
-      body class: "x-cloak bg-slate-200 w-auto flex flex-col justify-center space-y-4, mb-8" do
+      body class: "x-cloak bg-slate-200 w-auto flex flex-col justify-center space-y-4, mb-8", hx_headers: hx_headers do
         header class: "mx-auto" do
           link page_title, class: "h-full flex p-4 text-gray-500 text-2xl font-sans font-medium leading-normal", to: Home::Index, hx_boost: "true"
         end
@@ -22,25 +22,13 @@ abstract class MainLayout
           mount Shared::FlashMessages, context.flash
           content
         end
-        configure_htmx_request_script
       end
     end
   end
 
-  private def configure_htmx_request_script
-    tag "script", type: "text/javascript" do
-      raw <<-JS
-      document.body.addEventListener('htmx:configRequest', function(evt) {
-        var csrf_token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-        function csrfSafeMethod(method) {
-          // these HTTP methods do not require CSRF protection
-          return (/^(GET|HEAD|OPTIONS)$/i.test(method));
-        }
-        if (!csrfSafeMethod(evt.detail.verb)) {
-          evt.detail.headers["X-CSRF-TOKEN"] = csrf_token;
-        }
-      });
-      JS
-    end
+  private def hx_headers
+    {
+      Lucky::ProtectFromForgery::SESSION_KEY => Lucky::ProtectFromForgery.get_token(context)
+    }.to_json
   end
 end
