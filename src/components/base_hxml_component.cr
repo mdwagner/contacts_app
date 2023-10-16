10ec344307bd8ad0a11d07193a81f581897fdd7c
Michael Wagner <mdwranger@gmail.com>
2023-10-16 10:17:47 -0400

    fix: add back request helpers for hyperview

--- src/components/base_hxml_component.cr
+++ src/components/base_hxml_component.cr
@@ -1,2 +1,11 @@
 abstract class BaseHXMLComponent < LuckyHXML::Component
+  # Returns `true` if requesting platform is Android
+  def android? : Bool
+    context.request.headers["x-hyperview-platform"]? == "android"
+  end
+
+  # Returns `true` if requesting platform is iOS
+  def ios? : Bool
+    context.request.headers["x-hyperview-platform"]? == "ios"
+  end
 end
--- src/screens/main_screen.cr
+++ src/screens/main_screen.cr
@@ -74,4 +74,14 @@ abstract class MainScreen < LuckyHXML::Screen
       end
     end
   end
+
+  # Returns `true` if requesting platform is Android
+  def android? : Bool
+    context.request.headers["x-hyperview-platform"]? == "android"
+  end
+
+  # Returns `true` if requesting platform is iOS
+  def ios? : Bool
+    context.request.headers["x-hyperview-platform"]? == "ios"
+  end
 end
